d045a3ea9e9ba27b5ab2f9f4ef9fa0dfdb640653
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-01-22 20:39:02 -0500

    Broken Fs2 Native Example

---
+++ src/main/scala/com/example/concurrentlyfs2/fs2NativeStreamBroken.scala
@@ -0,0 +1,20 @@
+package com.example.concurrentlyfs2
+
+import cats.effect.IO
+import fs2.{StreamApp, Stream}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object fs2NativeStreamBroken extends StreamApp[IO] {
+
+  val bracketed : String => Stream[IO, StreamApp.ExitCode] = string =>  Stream.bracket(IO(println(s"Started $string")))(
+    using => Stream.eval_(IO.async[Unit](_ => ())) ,
+    release => IO(println(s"Release $string"))
+  )
+
+  def stream(args: List[String], requestShutdown: IO[Unit]) =
+    bracketed("Stream1")
+    .concurrently(bracketed("Stream2"))
+
+
+}
